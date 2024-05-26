a6324c4bd67dbdc4e634df23caabdfb39fa91d45
Ryan Pham <145263278+pookiesaurus@users.noreply.github.com>
2024-05-26 22:44:36 +1000

    looksmaxxing the index page

--- scripts/ryanscript.js
+++ scripts/ryanscript.js
@@ -4,21 +4,27 @@ window.addEventListener("scroll", function(){
     const cricketHeading = this.document.getElementById("titleheading")
     const navBar = this.document.getElementById('mainnavbar')
     const hamburger = this.document.getElementById('hamburger')
+    const credits = this.document.getElementById('credits')
 
 
 
     if (scrollPosition > 50){
         navBar.style.backgroundColor = "#238c41"
         cricketHeading.style.fontSize = '3vw'
+        cricketHeading.style.lineHeight = "10vh"
         navBar.style.height = '10vh'
         hamburger.style.opacity = '100%'
+        credits.style.opacity = "0"
+        credits.style.fontSize = "0.125vw"
 
     } else {
         navBar.style.backgroundColor = "#238c4100"
-        cricketHeading.style.fontSize = '20vw'
-        navBar.style.height = '35vh'
+        cricketHeading.style.fontSize = '15vw'
+        cricketHeading.style.lineHeight = "15vw"
+        navBar.style.height = '30vh'
         hamburger.style.opacity = '0'
-
+        credits.style.opacity = "100%"
+        credits.style.fontSize = "1.125vw"
     }
 })
 
@@ -32,3 +38,12 @@ function toggleMenu(){
         hamburgerMenu.style.width = '50vw'
     }
 }
+
+
+const sitePages = ["Content Hub", "Quiz yourself", "calculator"]
+
+function nextPagePreview() {}
+
+function prevPagePreview() {}
+
+function setContent() {}
